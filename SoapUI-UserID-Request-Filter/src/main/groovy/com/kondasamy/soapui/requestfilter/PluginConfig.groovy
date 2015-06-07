ea9ac09fe3f808971572a696f793f784693394b7
Kondasamy Jayaraman <jkondasamy1992@gmail.com>
2015-06-07 21:58:01 +0530

    SoapUI Request Filter Plugin
    SoapUI Request Filter Plugin - to detect anamolies in request sent.

---
+++ SoapUI-UserID-Request-Filter/src/main/groovy/com/kondasamy/soapui/requestfilter/PluginConfig.groovy
@@ -0,0 +1,10 @@
+package com.kondasamy.soapui.requestfilter;
+
+import com.eviware.soapui.plugins.PluginAdapter;
+import com.eviware.soapui.plugins.PluginConfiguration;
+
+@PluginConfiguration(groupId = "com.kondasamy.soapui.requestfilter.plugins", name = "SoapUI-UserID-Request-Filter SoapUI RequestFilter", version = "0.1",
+        autoDetect = true, description = "SoapUI-UserID-Request-Filter SoapUI RequestFilter",
+        infoUrl = "" )
+public class PluginConfig extends PluginAdapter {
+}
